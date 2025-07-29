32083f53ee861af4980d5148a51a8932e5ece2f7
brandonhood <brandon@atlashomebuyers.com>
2025-07-29 17:02:14 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,72 @@
 // Gulp.js configuration
-const gulp         = require("gulp");
-const sass         = require("gulp-sass");
-const autoprefixer = require("autoprefixer");
-const postcss      = require("gulp-postcss");
-const purgecss     = require("@fullhuman/postcss-purgecss");
-const browserSync  = require("browser-sync").create();
 
-// add this line:
-const fileInclude  = require("gulp-file-include");
+const // modules
+    gulp = require("gulp"),
+    // development mode?
+    sass = require("gulp-sass"),
+    autoprefixer = require("autoprefixer"),
+    postcss = require("gulp-postcss"),
+    purgecss = require("@fullhuman/postcss-purgecss"),
+    browserSync = require("browser-sync").create(),
 
-// Folders
-const src   = "src/";
+
+    // Folders
+   const src = "src/";
 const build = "./";
 
-// CSS processing (unchanged)
+// CSS processing
 function css() {
-  return gulp
-    .src(src + "scss/main.scss", { allowEmpty: true })
-    // …
-    .pipe(gulp.dest(build + "css/"))
-    .pipe(browserSync.reload({ stream: true }));
+    return gulp
+        .src(src + "scss/main.scss", { allowEmpty: true })
+        .pipe(
+            sass({
+                outputStyle: "nested",
+                imagePath: "/images/",
+                precision: 3,
+                errLogToConsole: true
+            }).on("error", sass.logError)
+        )
+        .pipe(
+            postcss([
+                purgecss({ content: ["**/*.html", "**/*.php"] }),
+                autoprefixer(),
+            ])
+        )
+        .pipe(gulp.dest(build + "css/"))
+        .pipe(browserSync.reload({ stream: true }));
 }
+exports.css = css;
 
-// HTML processing with file-includes
+// html processing
 function html() {
-  return gulp
-    .src(src + "*.html")              // pick up any HTML in src/
-    .pipe(fileInclude({
-      prefix: "@@",                   // use @@include("…")
-      basepath: "@file"               // look for partials next to each file
-    }))
-    .pipe(gulp.dest(build))           // write the output into your project root
-    .pipe(browserSync.stream());      // inject changes
+    gulp.task("html", function() {
+        return gulp.src(["**/*.html", "**/*.php"]);
+    });
 }
+exports.html = gulp.series(html, css);
 
-// expose tasks
-exports.css  = css;
-exports.html = html;
-
-// build both
-exports.build = gulp.parallel(css, html);
+// run all tasks
+exports.build = gulp.parallel(exports.css);
 
 // watch for file changes
-function watch() {
-  browserSync.init({
-    server: { baseDir: build },
-    tunnel: false
-  });
+function watch(done) {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        tunnel: false
+    });
 
-  gulp.watch(src + "scss/**/*", css);
-  gulp.watch([ src + "**/*.html", src + "partials/**/*.html" ], html);
+    // html changes
+    gulp.watch(["**/*.html", "**/*.php"], html).on("change",browserSync.reload);
+
+    // css changes
+    gulp.watch(src + "scss/**/*", css);
+
+    done();
 }
 
 exports.watch = watch;
-exports.default = gulp.series(exports.build, watch);
+
+// default task
+exports.default = gulp.series(exports.build, exports.watch);
